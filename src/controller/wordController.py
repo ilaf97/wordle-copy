9b0ef4b8c9c7edfb67e533b16dbee4c98c3b2259
Isaac.Frewin <isaac.frewin@virginmediao2.co.uk>
2024-11-05 23:01:47 +0000

    Ensure only admin can add new words to db

--- src/controller/wordController.py
+++ src/controller/wordController.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import os
 from typing import Any
 
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, render_template, request
+from flask_login import current_user
 from src.service import wordService
 from src.service.wordService import WordService
 from src.utils.exceptions import DatabaseError
+from utils.validations import Validators
 
 
 word = Blueprint('word', __name__)
@@ -23,7 +26,21 @@ def get_word(date: str) -> Any:
 
 
 	return f"<h1>{response.data}<h1>"
-	#return app.make_response(word)
 
+@word.route('/add-word', methods=['GET'])
 def add_word():
-	pass
+	if current_user.email != os.environ['ADMIN_EMAIL']:
+		return make_response("User not authorised to add words", 403)
+	return render_template('add_word.html')
+
+@word.route('/add-word', methods=['POST'])
+def add_word_post():
+	if current_user.email != os.environ['ADMIN_EMAIL']:
+		return make_response("User not authorised to add words", 403)
+	word = request.form.get('word')
+	valid_word = Validators.word(word) # type: ignore
+	if valid_word:
+		word_service.add_word(word) # type: ignore
+		return make_response(f"Word '{word}' added successfully")
+	return make_response(f"Word '{word}' is not valid", 400)
+	
